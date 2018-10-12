035f2559e68e72a11e1ecb890dcb44be91371104
Benjamin McLean <mclean.ben@gmail.com>
2018-10-12 08:18:03 -0500

    Added FetchModel

---
+++ src/main/java/warpwriter/fetch/FetchModel.java
@@ -0,0 +1,36 @@
+package warpwriter.fetch;
+
+/** Converts any IFetch to an IModel
+ * @author Ben McLean
+ */
+public class FetchModel implements IModel {
+    public IFetch fetch;
+    public int xSize, ySize, zSize;
+
+    public FetchModel(IFetch fetch, int xSize, int ySize, int zSize) {
+        this.fetch = fetch;
+        this.xSize = xSize;
+        this.ySize = ySize;
+        this.zSize = zSize;
+    }
+
+    @Override
+    public int xSize() {
+        return xSize;
+    }
+
+    @Override
+    public int ySize() {
+        return ySize;
+    }
+
+    @Override
+    public int zSize() {
+        return zSize;
+    }
+
+    @Override
+    public byte at(int x, int y, int z) {
+        return fetch.at(x, y, z);
+    }
+}
