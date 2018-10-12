4fae3f87f893c4cccfd3da57bfaf0836506b326a
Benjamin McLean <mclean.ben@gmail.com>
2018-10-12 18:34:01 -0500

    Added LoopModel

---
+++ src/main/java/warpwriter/model/LoopModel.java
@@ -0,0 +1,51 @@
+package warpwriter.model;
+
+/**
+ * Infinitely loop any fetch in any or all of the three dimensions.
+ *
+ * @author Ben McLean
+ */
+public class LoopModel implements IModel {
+    IFetch fetch;
+    int xSize, ySize, zSize;
+
+    /**
+     * @param fetch What to loop
+     * @param xSize Size to loop in x dimension, (from 0 inclusive to that size exclusive) or 0 to not loop in that dimension
+     * @param ySize Size to loop in y dimension, (from 0 inclusive to that size exclusive) or 0 to not loop in that dimension
+     * @param zSize Size to loop in z dimension, (from 0 inclusive to that size exclusive) or 0 to not loop in that dimension
+     */
+    public LoopModel(IFetch fetch, int xSize, int ySize, int zSize) {
+        this.fetch = fetch;
+        this.xSize = xSize;
+        this.ySize = ySize;
+        this.zSize = zSize;
+    }
+
+    @Override
+    public int xSize() {
+        return xSize;
+    }
+
+    @Override
+    public int ySize() {
+        return ySize;
+    }
+
+    @Override
+    public int zSize() {
+        return zSize;
+    }
+
+    @Override
+    public byte at(int x, int y, int z) {
+        return fetch.at(loop(x, xSize), loop(y, ySize), loop(z, zSize));
+    }
+
+    /**
+     * @param divisor Size of loop or 0 to turn off looping and just return dividend
+     */
+    public static int loop(int dividend, int divisor) {
+        return divisor == 0 ? dividend : dividend < 0 ? divisor - Math.abs(dividend % divisor) : dividend % divisor;
+    }
+}
