b82092de2e8f28a75cfddd1df4b9a0db219b0bf5
Benjamin McLean <mclean.ben@gmail.com>
2018-10-12 21:22:46 -0500

    Made some fetches

---
+++ src/main/java/warpwriter/model/ColorFetch.java
@@ -0,0 +1,33 @@
+package warpwriter.model;
+
+/**
+ * Always returns a solid color. Prevents creating more than 256 ColorFetch objects total.
+ *
+ * @author Ben McLean
+ */
+public class ColorFetch extends Fetch {
+    public static final ColorFetch transparent = colorFetch((byte) 0);
+    private static final ColorFetch colors[] = new ColorFetch[256];
+
+    /**
+     * Use this method instead of the constructor.
+     * @return A ColorFetch of color
+     */
+    public static ColorFetch colorFetch(byte color) {
+        if (colors[color] == null) {
+            colors[color] = new ColorFetch(color);
+        }
+        return colors[color];
+    }
+
+    private byte color;
+
+    private ColorFetch(byte color) {
+        this.color = color;
+    }
+
+    @Override
+    public byte at(int x, int y, int z) {
+        return color;
+    }
+}
---
+++ src/main/java/warpwriter/model/Fetch.java
@@ -0,0 +1,42 @@
+package warpwriter.model;
+
+/**
+ * This abstract class allows for IFetch implementations to defer to other IFetch instances for different coordinates instead of needing to return a byte.
+ * <p>
+ * The point of this class is that, to be useful, child classes must override either fetch(x, y, z) or at(x, y, z) or both. Methods which deal with this class will call fetch(x, y, z) first and if that is null, then call at(x, y, z) to get the answer.
+ *
+ * @author Ben McLean
+ */
+public abstract class Fetch implements IFetch {
+    /**
+     * This method is intended to be overridden with logic to determine which fill to return for the provided coordinate.
+     * <p>
+     * Returning null indicates to outside code that at(x, y, z) should be called instead.
+     *
+     * @return null
+     */
+    public Fetch fetch(int x, int y, int z) {
+        return null;
+    }
+
+    public Fetch fetch(int x, int z) {
+        return fetch(x, 0, z);
+    }
+
+    public Fetch fetch(int x) {
+        return fetch(x, 0);
+    }
+
+    @Override
+    public byte at(int x, int y, int z) {
+        return fetch(x, y, z).at(x, y, z);
+    }
+
+    public byte at(int x, int z) {
+        return fetch(x, z).at(x, z);
+    }
+
+    public byte at(int x) {
+        return fetch(x).at(x);
+    }
+}
