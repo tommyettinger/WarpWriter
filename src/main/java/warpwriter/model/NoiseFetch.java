f3f41e0788375030ce60b4a5b5d99cefbd2b136c
Benjamin McLean <mclean.ben@gmail.com>
2018-10-17 18:24:03 -0500

    Added overrides to prevent adding more after NoiseFetch too

--- src/main/java/warpwriter/model/NoiseFetch.java
+++ src/main/java/warpwriter/model/NoiseFetch.java
@@ -18,4 +18,26 @@ public class NoiseFetch extends Fetch {
     public byte bite(int x, int y, int z) {
         return colors[ModelMaker.hashBounded(x, y, z, seed, colors.length)];
     }
+
+    /**
+     * This is a final fetch. Anything added after it should be ignored.
+     *
+     * @param fetch Ignored
+     * @return this
+     */
+    @Override
+    public Fetch add(Fetch fetch) {
+        return this;
+    }
+
+    /**
+     * This is a final fetch. Anything added after it should be ignored.
+     *
+     * @param fetch Ignored
+     * @return this
+     */
+    @Override
+    public Fetch breakChain(Fetch fetch) {
+        return this;
+    }
 }
