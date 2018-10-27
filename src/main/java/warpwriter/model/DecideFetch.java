20fada326db6ddcd0a1f52303d1a8e30f02f49f7
Benjamin McLean <mclean.ben@gmail.com>
2018-10-26 20:08:22 -0500

    Added IDecide to abstract away decisions about different fetches into pure decisions usable by non-Fetch-related parts of programs

---
+++ src/main/java/warpwriter/model/DecideFetch.java
@@ -0,0 +1,38 @@
+package warpwriter.model;
+
+/**
+ * Uses an IDecide to make a choice between two different Fetches
+ *
+ * @author Ben McLean
+ */
+public class DecideFetch extends Fetch {
+    protected IDecide decide;
+    protected Fetch fetch;
+
+    public DecideFetch(IDecide decide, Fetch fetch) {
+        set(decide).set(fetch);
+    }
+
+    public DecideFetch set(IDecide decide) {
+        this.decide = decide;
+        return this;
+    }
+
+    public DecideFetch set(Fetch fetch) {
+        this.fetch = fetch;
+        return this;
+    }
+
+    public Fetch getFetch() {
+        return fetch;
+    }
+
+    public IDecide getDecide() {
+        return decide;
+    }
+
+    @Override
+    public Fetch fetch(int x, int y, int z) {
+        return decide.at(x, y, z) ? fetch : getNextFetch();
+    }
+}
---
+++ src/main/java/warpwriter/model/IDecide.java
@@ -0,0 +1,20 @@
+package warpwriter.model;
+
+/**
+ * An abstraction for any types that allow querying a 3D position to get a decision. (as a boolean)
+ * <br>
+ * Created by Ben McLean on 2018-10-26
+ *
+ * @author Ben McLean
+ */
+public interface IDecide {
+    /**
+     * Looks up a decision (a boolean) from a 3D position as x,y,z int parameters.
+     *
+     * @param x x position to look up; depending on angle, can be forward/back or left/right
+     * @param y y position to look up; depending on angle, can be left/right or forward/back
+     * @param z z position to look up; almost always up/down
+     * @return a decision
+     */
+    boolean at(int x, int y, int z);
+}
