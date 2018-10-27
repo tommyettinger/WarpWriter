df2e669232cd7ec759d3b1e19c370ad20ac5f1c7
Benjamin McLean <mclean.ben@gmail.com>
2018-10-27 17:29:33 -0500

    Update src/main/java/warpwriter/model/TurnFetch.java

---
+++ src/main/java/warpwriter/model/TurnFetch.java
@@ -0,0 +1,25 @@
+package warpwriter.model;
+
+/**
+ * Turns a fetch 90 degrees around 0, 0, 0
+ * @author Ben McLean
+ */
+public class TurnFetch extends Fetch {
+    private boolean clockwise;
+
+    public TurnFetch() {
+        this(true);
+    }
+
+    public TurnFetch (boolean clockwise) {
+        this.clockwise = clockwise;
+    }
+
+    public Fetch fetch(int x, int y, int z) {
+        if (clockwise)
+            setChains(x, z * -1, y);
+        else
+            setChains(x, z, y * -1);
+        return getNextFetch();
+    }
+}
