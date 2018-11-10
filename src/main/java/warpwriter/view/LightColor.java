cb93cb15f68f3c9914206430b9700e8024be0053
Benjamin McLean <mclean.ben@gmail.com>
2018-11-10 12:46:47 -0600

    Update src/main/java/warpwriter/view/LightColor.java

--- src/main/java/warpwriter/view/LightColor.java
+++ src/main/java/warpwriter/view/LightColor.java
@@ -20,20 +20,36 @@ public class LightColor implements IColor {
     public enum Direction {
         ABOVE_RIGHT, ABOVE_LEFT, BELOW_RIGHT, BELOW_LEFT;
 
-        public boolean above() {
+        public boolean isAbove() {
             return this == ABOVE_RIGHT || this == ABOVE_LEFT;
         }
 
-        public boolean below() {
-            return !above();
+        public boolean isBelow() {
+            return !isAbove();
         }
 
-        public boolean left() {
+        public boolean isLeft() {
             return this == ABOVE_LEFT || this == BELOW_LEFT;
         }
 
-        public boolean right() {
-            return !left();
+        public boolean isRight() {
+            return !isLeft();
+        }
+
+        public Direction above() {
+            return this == BELOW_RIGHT ? ABOVE_RIGHT : this == BELOW_LEFT ? ABOVE_LEFT : this;
+        }
+
+        public Direction below() {
+            return this == ABOVE_RIGHT ? BELOW_RIGHT : this == ABOVE_LEFT ? BELOW_LEFT : this;
+        }
+
+        public Direction left() {
+            return this == ABOVE_RIGHT ? ABOVE_LEFT : this == BELOW_RIGHT ? BELOW_LEFT : this;
+        }
+
+        public Direction right() {
+            return this == ABOVE_LEFT ? ABOVE_RIGHT : this == BELOW_LEFT ? BELOW_RIGHT : this;
         }
 
         public Direction zTurn() {
@@ -92,7 +108,7 @@ public class LightColor implements IColor {
             }
         }
 
-        public Direction uTurn() {
+        public Direction opposite() {
             switch (this) {
                 default:
                 case ABOVE_RIGHT:
@@ -118,8 +134,23 @@ public class LightColor implements IColor {
         return this;
     }
 
+    protected boolean vertVisible = true;
+
+    public boolean vertVisible() {
+        return vertVisible;
+    }
+
+    /**
+     * @param vertVisible Sets whether the vertical face (top or bottom) of the direction the light is pointing is visible from the angle being rendered from. This affects color choice. The left and right faces will use brighter colors if this is false.
+     * @return this
+     */
+    public LightColor set(boolean vertVisible) {
+        this.vertVisible = vertVisible;
+        return this;
+    }
+
     public int bright(byte voxel) {
-        return palette[(voxel & 255) - 1];
+        return palette[(voxel & 255) - 1]; // TODO: Make array safe!
     }
 
     public int normal(byte voxel) {
@@ -127,26 +158,30 @@ public class LightColor implements IColor {
     }
 
     public int dark(byte voxel) {
-        return palette[(voxel & 255) + 1];
+        return palette[(voxel & 255) + 1]; // TODO: Make array safe!
     }
 
     @Override
     public int topFace(byte voxel) {
-        return direction.above() ? bright(voxel) : dark(voxel);
+        return direction.isAbove() ? bright(voxel) : dark(voxel);
     }
 
     @Override
     public int bottomFace(byte voxel) {
-        return direction.below() ? bright(voxel) : dark(voxel);
+        return direction.isBelow() ? bright(voxel) : dark(voxel);
     }
 
     @Override
     public int leftFace(byte voxel) {
-        return direction.left() ? normal(voxel) : dark(voxel);
+        return vertVisible ?
+                direction.isLeft() ? normal(voxel) : dark(voxel)
+                : direction.isLeft() ? bright(voxel) : normal(voxel);
     }
 
     @Override
     public int rightFace(byte voxel) {
-        return direction.right() ? normal(voxel) : dark(voxel);
+        return vertVisible ?
+                direction.isRight() ? normal(voxel) : dark(voxel)
+                : direction.isRight() ? bright(voxel) : normal(voxel);
     }
 }
