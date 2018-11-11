76bacdce36cd3ebcc6bb935752e279365404e212
Tommy Ettinger <tommy.ettinger@gmail.com>
2018-11-10 18:15:19 -0800

    Use left/right voxel presence to determine shading

--- src/main/java/warpwriter/view/SimpleDraw.java
+++ src/main/java/warpwriter/view/SimpleDraw.java
@@ -30,16 +30,21 @@ public class SimpleDraw {
     public static void simpleDraw45(IModel model, IRenderer renderer, IVoxelColor color) {
         final int xSize = model.xSize(), ySize = model.ySize(), zSize = model.zSize();
         final int pixelWidth = xSize + ySize;
-        byte result;
+        byte result, right;
+        boolean leftVisible, // true when voxel at x - 1 == 0
+                rightVisible; // true when voxel at y + 1 == 0
         for (int px = 0; px < pixelWidth; px++) { // pixel x
             for (int py = 0; py < zSize; py++) { // pixel y
                 for (int vx = px - ySize + 1, vy = ySize - 1; vx < xSize && vy >= 0; vx++, vy--) {  // vx is voxel x, vy is voxel y
-                    if (model.at(vx - 1, vy, py) != 0 && model.at(vx, vy + 1, py) != 0) 
-                        result = model.at(vx, vy + 1, py);// we are inside the model here, don't try to render any further
+                    leftVisible = (model.at(vx - 1, vy, py)) == 0;
+                    rightVisible = (right = model.at(vx, vy + 1, py)) == 0;
+                    if (!leftVisible && !rightVisible) 
+                        result = right;// we are inside the model here because of how the diagonal passes through, but
+                        // it wouldn't be correct to show the model's guts; instead show an edge color we already found
                     else
                         result = model.at(vx, vy, py);
                     if (result != 0) {
-                        renderer.drawRect(px, (py), 1, 1, color.rightFace(result)); // "<<1" means "*2"
+                        renderer.drawRect(px, (py), 1, 1, rightVisible ? color.rightFace(result) : color.leftFace(result));
                         break;
                     }
                 }
