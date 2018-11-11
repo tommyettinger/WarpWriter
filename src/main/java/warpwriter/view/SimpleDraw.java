62cbc1fc42d1f55c57ef8bba61eb508fdcb7f831
Tommy Ettinger <tommy.ettinger@gmail.com>
2018-11-10 17:52:44 -0800

    Adjust how we halt when we would go inside a model to render

--- src/main/java/warpwriter/view/SimpleDraw.java
+++ src/main/java/warpwriter/view/SimpleDraw.java
@@ -30,12 +30,14 @@ public class SimpleDraw {
     public static void simpleDraw45(IModel model, IRenderer renderer, IVoxelColor color) {
         final int xSize = model.xSize(), ySize = model.ySize(), zSize = model.zSize();
         final int pixelWidth = xSize + ySize;
+        byte result;
         for (int px = 0; px < pixelWidth; px++) { // pixel x
             for (int py = 0; py < zSize; py++) { // pixel y
                 for (int vx = px - ySize + 1, vy = ySize - 1; vx < xSize && vy >= 0; vx++, vy--) {  // vx is voxel x, vy is voxel y
-                    if (model.at(vx - 1, vy, py) != 0 && model.at(vx, vy + 1, py) != 0)
-                        break; // we are inside the model here, don't try to render any further
-                    byte result = model.at(vx, vy, py);
+                    if (model.at(vx - 1, vy, py) != 0 && model.at(vx, vy + 1, py) != 0) 
+                        result = model.at(vx, vy + 1, py);// we are inside the model here, don't try to render any further
+                    else
+                        result = model.at(vx, vy, py);
                     if (result != 0) {
                         renderer.drawRect(px, (py), 1, 1, color.rightFace(result)); // "<<1" means "*2"
                         break;
