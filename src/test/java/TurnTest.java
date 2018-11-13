49e51e9a275ddbfe2ad9b62f18cdb7b817f768ed
Benjamin McLean <mclean.ben@gmail.com>
2018-11-12 20:37:22 -0600

    Trying to test LineDecide

--- src/test/java/TurnTest.java
+++ src/test/java/TurnTest.java
@@ -48,7 +48,8 @@ public class TurnTest extends ApplicationAdapter {
 
         turnModel = new TurnModel(
                 new DecideFetch(
-                        new PlaneDecide(1, 1, 1, 100).set(PlaneDecide.Condition.ON),
+//                        new PlaneDecide(1, 1, 1, 100).set(PlaneDecide.Condition.ON),
+                        new LineDecide(0, 0, 0, 100, 0, 100),
                         ColorFetch.color(Coloring.rinsed("Coastal Water 2"))
                 )
                         .boxModel(world,
