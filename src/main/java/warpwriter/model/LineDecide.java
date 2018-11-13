75e742571738cefc3ce943ef21db4dda50180e48
Benjamin McLean <mclean.ben@gmail.com>
2018-11-12 19:50:55 -0600

    Started making changes to LineDecide to make it point-based

--- src/main/java/warpwriter/model/LineDecide.java
+++ src/main/java/warpwriter/model/LineDecide.java
@@ -4,52 +4,52 @@ package warpwriter.model;
  * @author Ben McLean
  */
 public class LineDecide implements IDecide {
-    protected int a1, b1, c1, a2, b2, c2;
+    protected int x1, y1, z1, x2, y2, z2;
 
-    public int a1() {
-        return a1;
+    public int x1() {
+        return x1;
     }
 
-    public int a2() {
-        return a2;
+    public int x2() {
+        return x2;
     }
 
-    public int b1() {
-        return b1;
+    public int y1() {
+        return y1;
     }
 
-    public int b2() {
-        return b2;
+    public int y2() {
+        return y2;
     }
 
-    public int c1() {
-        return c1;
+    public int z1() {
+        return z1;
     }
 
-    public int c2() {
-        return c2;
+    public int z2() {
+        return z2;
     }
 
-    public LineDecide setPlane1(int a1, int b1, int c1) {
-        this.a1 = a1;
-        this.b1 = b1;
-        this.c1 = c1;
+    public LineDecide setPoint1(int x1, int y1, int z1) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.z1 = z1;
         return this;
     }
 
-    public LineDecide setPlane2(int a2, int b2, int c2) {
-        this.a2 = a2;
-        this.b2 = b2;
-        this.c2 = c2;
+    public LineDecide setPoint2(int x2, int y2, int z2) {
+        this.x2 = x2;
+        this.y2 = y2;
+        this.z2 = z2;
         return this;
     }
 
-    public LineDecide(int a1, int b1, int c1, int a2, int b2, int c2) {
-        setPlane1(a1, b1, c1).setPlane2(a2, b2, c2);
+    public LineDecide(int x1, int y1, int z1, int x2, int y2, int z2) {
+        setPoint1(x1, y1, z1).setPoint2(x2, y2, z2);
     }
 
     @Override
     public boolean bool(int x, int y, int z) {
-        return x * a1 + y * b1 + z * c1 == 0 && x * a2 + y * b2 + z * c2 == 0;
+        return x * this.x1 + y * y1 + z * z1 == 0 && x * x2 + y * y2 + z * z2 == 0;
     }
 }
