2f04db95b32877039ce2dce71a638a1b889af4e2
Benjamin McLean <mclean.ben@gmail.com>
2018-11-25 15:14:59 -0600

    Fixed upper right triangles

--- src/main/java/warpwriter/view/SimpleDraw.java
+++ src/main/java/warpwriter/view/SimpleDraw.java
@@ -183,16 +183,15 @@ public class SimpleDraw {
             // Finish drawing main bulk of model
 
             // Begin drawing top triangles
-            if (px - 2 > sizeVY2) {
-                // TODO: Fix upper right triangles
-                result = model.at(sizeVX - 1, (px - sizeVY2) / 2, sizeVZ - 1);
-                if (result != 0) {
-                    renderer.drawRightTriangle(px, topPY, flash(color.topFace(result)));
-                }
-            } else if (sizeVY % 2 == 0 || sizeVY2 != px + 2) { // Prevent extra triangle from appearing on top
+            if (px + 2 < sizeVY2) { // Top left triangles
                 result = model.at(0, px / 2, sizeVZ - 1);
                 if (result != 0) {
                     renderer.drawLeftTriangle(px + 2, topPY, color.topFace(result));
+                }
+            } else if (px + 2 > sizeVY2) { // Top right triangles
+                result = model.at((px - sizeVY2) / 2, sizeVY - 1, sizeVZ - 1);
+                if (result != 0) {
+                    renderer.drawRightTriangle(px, topPY, color.topFace(result));
                 }
             }
             // Finish drawing top triangles.
