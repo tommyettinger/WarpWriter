708791e8614285d13f6fbfe890cfefc74eea4e2d
Benjamin McLean <mclean.ben@gmail.com>
2019-05-21 07:27:56 -0500

    Turning model around before spin makes first frame have better angle

--- src/test/java/PreviewSpinnerTest.java
+++ src/test/java/PreviewSpinnerTest.java
@@ -134,6 +134,7 @@ public class PreviewSpinnerTest extends ApplicationAdapter {
     public Pixmap[] spin(IModel models[]) {
         final int width = width(models) * 2, height = height(models);
         final TurnModel turnModel = new TurnModel();
+        turnModel.turner().clockZ().clockZ();
         Pixmap[] result = new Pixmap[models.length * 8];
         for (int z = 0; z < 4; z++) {
             for (int model = 0; model < models.length; model++) {
