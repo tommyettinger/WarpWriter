ccc6fb86a85dbd5e674bf28b9c74651c5f4b43c3
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-05-24 23:06:53 -0700

    Nice new shader helps when skin tones are too dithered

--- src/main/java/warpwriter/view/render/ShaderUtils.java
+++ src/main/java/warpwriter/view/render/ShaderUtils.java
@@ -164,6 +164,50 @@ public class ShaderUtils {
     //                    "   gl_FragColor.rgb = v_color.rgb * clamp(vec3(dot(tgt.rgb, vec3(1.0, 0.625, -0.5)), dot(tgt.rgb, vec3(1.0, -0.375, 0.5)), dot(tgt.rgb, vec3(1.0, -0.375, -0.5))), 0.0, 1.0);\n" +
 
     /**
+     * This fragment shader substitutes colors with ones from a palette, acting like {@link #fragmentShader} but also
+     * allowing color space adjustments to be done after the palette swap (this won't change the color count). This
+     * shader also "softens" colors that are warmer, making dithering affect them less strongly (useful for skin tones
+     * and some other types of material). The uniforms {@code u_mul} and {@code u_add} are each YCwCm adjustments. The
+     * first multiplies the Y (brightness), Cw (Chroma warm, with values greater than 1 making warm colors warmer and
+     * cool colors cooler) and Cm (Chroma mild, with values greater than 1 making green/yellow colors closer to those
+     * and red/blue colors closer to that) by the image's YCwCm values after palette-substitution. After that,
+     * {@code u_add} is added to Y (which can have an internal value between 0 and 1, and all are clamped), Cw (which
+     * ranges between -1 for blue/green and 1 for red/yellow), and Cm (which ranges between -1 for red/blue and 1 for
+     * yellow/green). You can use this to desaturate colors by setting {@code u_mul} to {@code vec3(1.0, 0.5, 0.5)} or
+     * any other small fractions for Cw and Cm. You can make colors warmer by setting {@code u_add} to
+     * {@code vec3(0.0, 0.6, 0.0)}; while warmth is added, randomly setting Cm to a value between -0.5 and 0.5 can
+     * simulate a fiery color effect over the screen. You can make an icy effect by setting {@code u_add} to
+     * {@code vec3(0.3, -0.4, 0.0)}.
+     * <br>
+     * This is a variant on {@link #fragmentShaderWarmMildLimited} that will also only produce colors from one palette,
+     * with a max of 255 colors plus transparent, but can change the lightness of warm-color areas and reduce their
+     * dithering intensity.
+     */
+    public static final String fragmentShaderWarmMildSoft =
+            "varying vec2 v_texCoords;\n" +
+                    "varying vec4 v_color;\n" +
+                    "uniform sampler2D u_texture;\n" +
+                    "uniform sampler2D u_palette;\n" +
+                    "uniform vec3 u_add;\n" +
+                    "uniform vec3 u_mul;\n" +
+                    "const float b_adj = 31.0 / 32.0;\n" +
+                    "const float rb_adj = 32.0 / 1023.0;\n" +
+                    "void main()\n" +
+                    "{\n" +
+                    "   vec4 tgt = texture2D( u_texture, v_texCoords );\n" +
+                    "   vec4 used = texture2D(u_palette, vec2((tgt.b * b_adj + floor(tgt.r * 31.999)) * rb_adj, 1.0 - tgt.g));\n" +
+                    "   float len = length(tgt.rgb) + 1.0;\n" +
+//                    "   float len = abs((tgt.r - tgt.b) * (tgt.g - tgt.b)) * 4.0;\n" +
+                    "   float adj = fract(52.9829189 * fract(dot(vec2(0.06711056, 0.00583715), gl_FragCoord.xy))) * len - len * 0.5;\n" +
+                    "   tgt.rgb = clamp(tgt.rgb + (tgt.rgb - used.rgb) * adj + (tgt.r - tgt.b + 0.6) * 0.04, 0.0, 1.0);\n" +
+                    "   tgt.rgb = u_add + u_mul * vec3(dot(tgt.rgb, vec3(0.375, 0.5, 0.125)), tgt.r - tgt.b, tgt.g - tgt.b);\n" +
+                    "   tgt.rgb = clamp(vec3(dot(tgt.rgb, vec3(1.0, 0.625, -0.5)), dot(tgt.rgb, vec3(1.0, -0.375, 0.5)), dot(tgt.rgb, vec3(1.0, -0.375, -0.5))), 0.0, 1.0);\n" +
+                    "   gl_FragColor.rgb = v_color.rgb * texture2D(u_palette, vec2((tgt.b * b_adj + floor(tgt.r * 31.999)) * rb_adj, 1.0 - tgt.g)).rgb;\n" +
+                    "   gl_FragColor.a = v_color.a * tgt.a;\n" +
+                    "}";
+
+
+    /**
      * This fragment shader substitutes colors with ones from a palette, dithering as needed using a variant on the R2
      * with triangle wave dithering technique suggested by Martin Roberts. This shows line artifacts in some places,
      * aligned to a rhombic grid (matching the lines in isometric pixel art, interestingly).
