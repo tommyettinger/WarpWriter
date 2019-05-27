bb6a42f04311463f47a1a269b7e4224eb6e7043e
Benjamin McLean <mclean.ben@gmail.com>
2019-05-27 16:46:03 -0500

    Fixed issue with east and west being backwards

--- src/main/java/warpwriter/model/nonvoxel/Rotation.java
+++ src/main/java/warpwriter/model/nonvoxel/Rotation.java
@@ -9,30 +9,30 @@ import squidpony.StringKit;
  */
 public enum Rotation implements ITurnable {
     SOUTH0(0, "SOUTH0", -1, 1, 2),
-    SOUTH1(1, "SOUTH1", -1, -3, 1),
+    SOUTH1(1, "SOUTH1", -1, 2, -2),
     SOUTH2(2, "SOUTH2", -1, -2, -3),
-    SOUTH3(3, "SOUTH3", -1, 2, -2),
-    EAST0(4, "EAST0", -2, -1, 2),
-    EAST1(5, "EAST1", -2, -3, -1),
-    EAST2(6, "EAST2", -2, 0, -3),
-    EAST3(7, "EAST3", -2, 2, 0),
+    SOUTH3(3, "SOUTH3", -1, -3, 1),
+    WEST0(4, "WEST0", -2, -1, 2),
+    WEST1(5, "WEST1", -2, 2, 0),
+    WEST2(6, "WEST2", -2, 0, -3),
+    WEST3(7, "WEST3", -2, -3, -1),
     NORTH0(8, "NORTH0", 0, -2, 2),
-    NORTH1(9, "NORTH1", 0, -3, -2),
+    NORTH1(9, "NORTH1", 0, 2, 1),
     NORTH2(10, "NORTH2", 0, 1, -3),
-    NORTH3(11, "NORTH3", 0, 2, 1),
-    WEST0(12, "WEST0", 1, 0, 2),
-    WEST1(13, "WEST1", 1, -3, 0),
-    WEST2(14, "WEST2", 1, -1, -3),
-    WEST3(15, "WEST3", 1, 2, -1),
+    NORTH3(11, "NORTH3", 0, -3, -2),
+    EAST0(12, "EAST0", 1, 0, 2),
+    EAST1(13, "EAST1", 1, 2, -1),
+    EAST2(14, "EAST2", 1, -1, -3),
+    EAST3(15, "EAST3", 1, -3, 0),
     UP0(16, "UP0", -3, -2, 0),
-    UP1(17, "UP1", -3, -1, -2),
+    UP1(17, "UP1", -3, 0, 1),
     UP2(18, "UP2", -3, 1, -1),
-    UP3(19, "UP3", -3, 0, 1),
+    UP3(19, "UP3", -3, -1, -2),
     DOWN0(20, "DOWN0", 2, -2, -1),
-    DOWN1(21, "DOWN1", 2, 0, -2),
+    DOWN1(21, "DOWN1", 2, -1, 1),
     DOWN2(22, "DOWN2", 2, 1, 0),
-    DOWN3(23, "DOWN3", 2, -1, 1);
-
+    DOWN3(23, "DOWN3", 2, 0, -2);
+    
     public static final Rotation[] rotations = new Rotation[24];
 
     static {
@@ -68,52 +68,52 @@ public enum Rotation implements ITurnable {
         switch (this) {
             default:
             case SOUTH0:
-                return SOUTH1;
-            case SOUTH1:
-                return SOUTH2;
-            case SOUTH2:
                 return SOUTH3;
             case SOUTH3:
+                return SOUTH2;
+            case SOUTH2:
+                return SOUTH1;
+            case SOUTH1:
                 return SOUTH0;
-            case EAST0:
-                return EAST1;
-            case EAST1:
-                return EAST2;
-            case EAST2:
-                return EAST3;
-            case EAST3:
-                return EAST0;
-            case NORTH0:
-                return NORTH1;
-            case NORTH1:
-                return NORTH2;
-            case NORTH2:
-                return NORTH3;
-            case NORTH3:
-                return NORTH0;
             case WEST0:
-                return WEST1;
-            case WEST1:
-                return WEST2;
-            case WEST2:
                 return WEST3;
             case WEST3:
+                return WEST2;
+            case WEST2:
+                return WEST1;
+            case WEST1:
                 return WEST0;
+            case NORTH0:
+                return NORTH3;
+            case NORTH3:
+                return NORTH2;
+            case NORTH2:
+                return NORTH1;
+            case NORTH1:
+                return NORTH0;
+            case EAST0:
+                return EAST3;
+            case EAST3:
+                return EAST2;
+            case EAST2:
+                return EAST1;
+            case EAST1:
+                return EAST0;
             case UP0:
-                return UP1;
-            case UP1:
-                return UP2;
-            case UP2:
                 return UP3;
             case UP3:
+                return UP2;
+            case UP2:
+                return UP1;
+            case UP1:
                 return UP0;
             case DOWN0:
-                return DOWN1;
-            case DOWN1:
-                return DOWN2;
-            case DOWN2:
                 return DOWN3;
             case DOWN3:
+                return DOWN2;
+            case DOWN2:
+                return DOWN1;
+            case DOWN1:
                 return DOWN0;
         }
     }
@@ -124,52 +124,52 @@ public enum Rotation implements ITurnable {
             default:
             case SOUTH0:
                 return DOWN2;
-            case SOUTH1:
-                return WEST1;
-            case SOUTH2:
-                return UP0;
             case SOUTH3:
                 return EAST3;
-            case EAST0:
-                return DOWN3;
-            case EAST1:
-                return SOUTH1;
-            case EAST2:
-                return UP3;
-            case EAST3:
-                return NORTH3;
-            case NORTH0:
-                return DOWN0;
-            case NORTH1:
-                return EAST1;
-            case NORTH2:
-                return UP2;
-            case NORTH3:
-                return WEST3;
+            case SOUTH2:
+                return UP0;
+            case SOUTH1:
+                return WEST1;
             case WEST0:
                 return DOWN1;
-            case WEST1:
-                return NORTH1;
-            case WEST2:
-                return UP1;
             case WEST3:
                 return SOUTH3;
+            case WEST2:
+                return UP1;
+            case WEST1:
+                return NORTH1;
+            case NORTH0:
+                return DOWN0;
+            case NORTH3:
+                return WEST3;
+            case NORTH2:
+                return UP2;
+            case NORTH1:
+                return EAST1;
+            case EAST0:
+                return DOWN3;
+            case EAST3:
+                return NORTH3;
+            case EAST2:
+                return UP3;
+            case EAST1:
+                return SOUTH1;
             case UP0:
                 return NORTH0;
-            case UP1:
-                return EAST0;
-            case UP2:
-                return SOUTH0;
             case UP3:
                 return WEST0;
+            case UP2:
+                return SOUTH0;
+            case UP1:
+                return EAST0;
             case DOWN0:
                 return SOUTH2;
-            case DOWN1:
-                return EAST2;
-            case DOWN2:
-                return NORTH2;
             case DOWN3:
                 return WEST2;
+            case DOWN2:
+                return NORTH2;
+            case DOWN1:
+                return EAST2;
         }
     }
 
@@ -178,53 +178,53 @@ public enum Rotation implements ITurnable {
         switch (this) {
             default:
             case SOUTH0:
-                return EAST0;
-            case SOUTH1:
-                return DOWN3;
-            case SOUTH2:
-                return WEST2;
-            case SOUTH3:
-                return UP1;
-            case EAST0:
-                return NORTH0;
-            case EAST1:
-                return DOWN0;
-            case EAST2:
-                return SOUTH2;
-            case EAST3:
-                return UP0;
-            case NORTH0:
                 return WEST0;
-            case NORTH1:
+            case SOUTH3:
                 return DOWN1;
-            case NORTH2:
+            case SOUTH2:
                 return EAST2;
-            case NORTH3:
+            case SOUTH1:
                 return UP3;
             case WEST0:
-                return SOUTH0;
-            case WEST1:
-                return DOWN2;
-            case WEST2:
-                return NORTH2;
+                return NORTH0;
             case WEST3:
+                return DOWN0;
+            case WEST2:
+                return SOUTH2;
+            case WEST1:
+                return UP0;
+            case NORTH0:
+                return EAST0;
+            case NORTH3:
+                return DOWN3;
+            case NORTH2:
+                return WEST2;
+            case NORTH1:
+                return UP1;
+            case EAST0:
+                return SOUTH0;
+            case EAST3:
+                return DOWN2;
+            case EAST2:
+                return NORTH2;
+            case EAST1:
                 return UP2;
             case UP0:
-                return WEST1;
-            case UP1:
-                return NORTH1;
-            case UP2:
-                return EAST1;
-            case UP3:
-                return SOUTH1;
-            case DOWN0:
-                return WEST3;
-            case DOWN1:
-                return SOUTH3;
-            case DOWN2:
                 return EAST3;
-            case DOWN3:
+            case UP3:
                 return NORTH3;
+            case UP2:
+                return WEST3;
+            case UP1:
+                return SOUTH3;
+            case DOWN0:
+                return EAST1;
+            case DOWN3:
+                return SOUTH1;
+            case DOWN2:
+                return WEST1;
+            case DOWN1:
+                return NORTH1;
         }
     }
 
@@ -233,52 +233,52 @@ public enum Rotation implements ITurnable {
         switch (this) {
             default:
             case SOUTH0:
-                return SOUTH3;
-            case SOUTH1:
-                return SOUTH0;
-            case SOUTH2:
                 return SOUTH1;
             case SOUTH3:
+                return SOUTH0;
+            case SOUTH2:
+                return SOUTH3;
+            case SOUTH1:
                 return SOUTH2;
-            case EAST0:
-                return EAST3;
-            case EAST1:
-                return EAST0;
-            case EAST2:
-                return EAST1;
-            case EAST3:
-                return EAST2;
-            case NORTH0:
-                return NORTH3;
-            case NORTH1:
-                return NORTH0;
-            case NORTH2:
-                return NORTH1;
-            case NORTH3:
-                return NORTH2;
             case WEST0:
-                return WEST3;
-            case WEST1:
-                return WEST0;
-            case WEST2:
                 return WEST1;
             case WEST3:
+                return WEST0;
+            case WEST2:
+                return WEST3;
+            case WEST1:
                 return WEST2;
+            case NORTH0:
+                return NORTH1;
+            case NORTH3:
+                return NORTH0;
+            case NORTH2:
+                return NORTH3;
+            case NORTH1:
+                return NORTH2;
+            case EAST0:
+                return EAST1;
+            case EAST3:
+                return EAST0;
+            case EAST2:
+                return EAST3;
+            case EAST1:
+                return EAST2;
             case UP0:
-                return UP3;
-            case UP1:
-                return UP0;
-            case UP2:
                 return UP1;
             case UP3:
+                return UP0;
+            case UP2:
+                return UP3;
+            case UP1:
                 return UP2;
             case DOWN0:
-                return DOWN3;
-            case DOWN1:
-                return DOWN0;
-            case DOWN2:
                 return DOWN1;
             case DOWN3:
+                return DOWN0;
+            case DOWN2:
+                return DOWN3;
+            case DOWN1:
                 return DOWN2;
         }
     }
@@ -289,52 +289,52 @@ public enum Rotation implements ITurnable {
             default:
             case SOUTH0:
                 return UP2;
-            case SOUTH1:
-                return EAST1;
-            case SOUTH2:
-                return DOWN0;
             case SOUTH3:
                 return WEST3;
-            case EAST0:
-                return UP1;
-            case EAST1:
-                return NORTH1;
-            case EAST2:
-                return DOWN1;
-            case EAST3:
-                return SOUTH3;
-            case NORTH0:
-                return UP0;
-            case NORTH1:
-                return WEST1;
-            case NORTH2:
-                return DOWN2;
-            case NORTH3:
-                return EAST3;
+            case SOUTH2:
+                return DOWN0;
+            case SOUTH1:
+                return EAST1;
             case WEST0:
                 return UP3;
-            case WEST1:
-                return SOUTH1;
-            case WEST2:
-                return DOWN3;
             case WEST3:
                 return NORTH3;
+            case WEST2:
+                return DOWN3;
+            case WEST1:
+                return SOUTH1;
+            case NORTH0:
+                return UP0;
+            case NORTH3:
+                return EAST3;
+            case NORTH2:
+                return DOWN2;
+            case NORTH1:
+                return WEST1;
+            case EAST0:
+                return UP1;
+            case EAST3:
+                return SOUTH3;
+            case EAST2:
+                return DOWN1;
+            case EAST1:
+                return NORTH1;
             case UP0:
                 return SOUTH2;
-            case UP1:
-                return WEST2;
-            case UP2:
-                return NORTH2;
             case UP3:
                 return EAST2;
+            case UP2:
+                return NORTH2;
+            case UP1:
+                return WEST2;
             case DOWN0:
                 return NORTH0;
-            case DOWN1:
-                return WEST0;
-            case DOWN2:
-                return SOUTH0;
             case DOWN3:
                 return EAST0;
+            case DOWN2:
+                return SOUTH0;
+            case DOWN1:
+                return WEST0;
         }
     }
 
@@ -343,53 +343,53 @@ public enum Rotation implements ITurnable {
         switch (this) {
             default:
             case SOUTH0:
-                return WEST0;
-            case SOUTH1:
-                return UP3;
-            case SOUTH2:
-                return EAST2;
-            case SOUTH3:
-                return DOWN1;
-            case EAST0:
-                return SOUTH0;
-            case EAST1:
-                return UP2;
-            case EAST2:
-                return NORTH2;
-            case EAST3:
-                return DOWN2;
-            case NORTH0:
                 return EAST0;
-            case NORTH1:
+            case SOUTH3:
                 return UP1;
-            case NORTH2:
+            case SOUTH2:
                 return WEST2;
-            case NORTH3:
+            case SOUTH1:
                 return DOWN3;
             case WEST0:
-                return NORTH0;
-            case WEST1:
-                return UP0;
-            case WEST2:
-                return SOUTH2;
+                return SOUTH0;
             case WEST3:
+                return UP2;
+            case WEST2:
+                return NORTH2;
+            case WEST1:
+                return DOWN2;
+            case NORTH0:
+                return WEST0;
+            case NORTH3:
+                return UP3;
+            case NORTH2:
+                return EAST2;
+            case NORTH1:
+                return DOWN1;
+            case EAST0:
+                return NORTH0;
+            case EAST3:
+                return UP0;
+            case EAST2:
+                return SOUTH2;
+            case EAST1:
                 return DOWN0;
             case UP0:
-                return EAST3;
-            case UP1:
-                return SOUTH3;
-            case UP2:
-                return WEST3;
-            case UP3:
-                return NORTH3;
-            case DOWN0:
-                return EAST1;
-            case DOWN1:
-                return NORTH1;
-            case DOWN2:
                 return WEST1;
-            case DOWN3:
+            case UP3:
                 return SOUTH1;
+            case UP2:
+                return EAST1;
+            case UP1:
+                return NORTH1;
+            case DOWN0:
+                return WEST3;
+            case DOWN3:
+                return NORTH3;
+            case DOWN2:
+                return EAST3;
+            case DOWN1:
+                return SOUTH3;
         }
     }
 
