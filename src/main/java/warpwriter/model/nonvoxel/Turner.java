31594456b3b52395f28b3b7165dc9af495290adb
Benjamin McLean <mclean.ben@gmail.com>
2019-05-27 15:08:32 -0500

    Copied some code out of old Turner into new rotation enum

--- src/main/java/warpwriter/model/nonvoxel/Turner.java
+++ src/main/java/warpwriter/model/nonvoxel/Turner.java
@@ -50,6 +50,10 @@ public class Turner implements ITurner {
             return rotation;
         }
 
+        public int rotation(int index) {
+            return rotation[index];
+        }
+
         public final int value() {
             return value;
         }
@@ -422,6 +426,33 @@ public class Turner implements ITurner {
                     return value;
             }
             throw new IllegalArgumentException("Rotation array " + StringKit.join(", ", rotation) + " does not correspond to a rotation.");
+        }
+
+        public int affected(int axis) {
+            return flipBits(rotation(axis));
+        }
+
+        /**
+         * Does a reverse lookup on the rotation array for the axis affected by the rotation
+         *
+         * @param axis 0 or -1 for x, 1 or -2 for y, 2 or -3 for z
+         * @return Which axis the specified axis was before the rotation. 0 for x, 1 for y, 2 for z.
+         */
+        public int reverseLookup(int axis) {
+            final int index = flipBits(axis);
+            for (int rot = 0; rot < rotation.length; rot++)
+                if (index == affected(rot))
+                    return rot;
+            throw new IllegalStateException();
+        }
+
+        /**
+         * Flips the bits in rot if rot is negative, leaves it alone if positive. Where x, y, and z correspond to elements
+         * 0, 1, and 2 in a rotation array, if those axes are reversed we use {@code ~0}, or -1, for x, and likewise -2 and
+         * -3 for y and z when reversed.
+         */
+        public static int flipBits(int rot) {
+            return rot ^ rot >> 31; // (rot ^ rot >> 31) is roughly equal to (rot < 0 ? -1 - rot : rot)
         }
     }
 
